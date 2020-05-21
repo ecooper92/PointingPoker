e3846747b99e2851220e6d011213a1570f2b7b26
Eric Cooper <e.cooper92@gmail.com>
2020-05-21 17:33:36 -0500

    Added Participant Type

--- PointingPoker/Data/Participant.cs
+++ PointingPoker/Data/Participant.cs
@@ -4,14 +4,17 @@ namespace PointingPoker.Data
 {
     public class Participant
     {
-        public Participant(string userId, string name)
+        public Participant(string userId, string name, ParticipantType type)
         {
             UserId = userId;
             Name = name;
+            Type = type;
         }
 
         public string UserId { get; } = string.Empty;
 
         public string Name { get; } = string.Empty;
+
+        public ParticipantType Type { get; } = ParticipantType.Observer;
     }
 }
